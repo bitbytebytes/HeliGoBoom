3c07bd05d88039fa896e7175ec3b7354f0dc5e60
bitbytebytes <bytebit@bitbytebytes.io>
2026-04-21 11:29:14 -0400

    initial commit

---
+++ Main.gd
@@ -0,0 +1,20 @@
+extends Node
+
+
+func _ready():
+    setup_mod()
+    queue_free()
+
+
+func setup_mod():
+    overrideScript()
+    log_message("HeliGoBoom: override complete.")
+
+
+func overrideScript():
+    var new_script = load('res://mods/HeliGoBoom/heli_go_boom.gd')
+    new_script.take_over_path("res://Scripts/EventSystem.gd")
+    
+
+func log_message(message: String) -> void:
+    print("[HeliGoBoom]: " + message)
---
+++ heli_go_boom.gd
@@ -0,0 +1,37 @@
+extends "res://Scripts/EventSystem.gd"
+
+
+func CrashSite():
+    var randomCrash = crashes.get_child(randi_range(0, crashes.get_child_count() - 1))
+
+    var crashSite = crash.instantiate()
+    
+    var event_start_timer = Timer.new()
+    var event_delay_time = randf() * 300.0
+    event_start_timer.wait_time = event_delay_time
+    event_start_timer.one_shot = true
+    event_start_timer.autostart = true
+    
+    var boom_player  = AudioStreamPlayer3D.new()
+    boom_player.stream = load("res://mods/HeliGoBoom/Audio/Helicopter/heli_crash.mp3")
+    boom_player.bus = "SFX"
+    boom_player.max_distance = 1000.0
+    boom_player.max_db = 12.0
+    boom_player.volume_db = 12.0
+    boom_player.autoplay = true
+    
+    add_child(event_start_timer)
+    await event_start_timer.timeout
+    event_start_timer.queue_free()
+    
+    randomCrash.add_child(boom_player)
+    await boom_player.finished
+    
+    randomCrash.add_child(crashSite)
+    crashSite.global_transform = randomCrash.global_transform
+    
+
+
+func _unhandled_key_input(event: InputEvent) -> void:
+    if Input.is_key_pressed(KEY_BACKSLASH):
+        CrashSite()
